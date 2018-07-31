e4f12df824aeccb89aee5ed9d8460bce8621d7a2
Cristina <>
2018-07-31 13:58:48 +0200

    add posts index

--- addons/cargoweb/flyingpigs/cargo-theme/resources/js/all.js
+++ addons/cargoweb/flyingpigs/cargo-theme/resources/js/all.js
@@ -42,6 +42,20 @@ $(window).on("load", function(){
       });
 
   });
+
+    $('[data-modal-open]').on('click', function(){
+        var modalSelector = $(this).data('modal-open');
+        $('body').addClass('modal-open');
+        $('[data-modal='+ modalSelector +']').addClass('modal-open');
+    });
+
+    $('[data-modal-close]').on('click', function(ev){
+        if (this === ev.target){
+            $('body').removeClass('modal-open');
+            $('[data-modal]').removeClass('modal-open');
+        }
+    });
+
 });
 
 $(window).on("resize", function(){
