5801e44f2a86a53fda0c4a05589c33d2a891237c
cris <cristinag@flyingpigs.es>
2018-08-17 11:59:56 +0200

    Revert " header mobile"
    This reverts commit eb693319ae31460a80d6b45ff644ea50d7e70228.

--- addons/cargoweb/flyingpigs/cargo-theme/resources/js/all.js
+++ addons/cargoweb/flyingpigs/cargo-theme/resources/js/all.js
@@ -59,53 +59,6 @@ $(window).on("load", function () {
     $(this).closest("." + selector).hide();
   });
 
-   // Detect IE version
-   var iev=0;
-   var ieold = (/MSIE (\d+\.\d+);/.test(navigator.userAgent));
-   var trident = !!navigator.userAgent.match(/Trident\/7.0/);
-   var rv=navigator.userAgent.indexOf("rv:11.0");
-
-   if (ieold) iev=new Number(RegExp.$1);
-   if (navigator.appVersion.indexOf("MSIE 10") != -1) iev=10;
-   if (trident&&rv!=-1) iev=11;
-
-   // Firefox or IE 11
-   if(typeof InstallTrigger !== 'undefined' || iev == 11) {
-       var lastScrollTop = 0;
-       $(window).on('scroll', function() {
-           st = $(this).scrollTop();
-           if(st < lastScrollTop) {
-             $('.shrink-on-scroll').addClass('big-padding');
-          }
-          else if(st > lastScrollTop) {
-            $('.shrink-on-scroll').removeClass('big-padding');
-           }
-           lastScrollTop = st;
-       });
-   }
-   // Other browsers
-   else {
-       $('body').on('mousewheel', function(e){
-           if(e.originalEvent.wheelDelta > 0) {
-             $('.shrink-on-scroll').addClass('big-padding');
-          }
-          else if(e.originalEvent.wheelDelta < 0) {
-            $('.shrink-on-scroll').removeClass('big-padding');
-           }
-       });
-   }
-
-   var hash = window.location.hash;
-   if(hash){
-    var headerOffset = $('header.header').height()+ 100;
-    var target = $(hash);
-    var offsetTop = parseInt($($(this).data('offsettop'))[0]) > 0 ? parseInt($($(this).data('offsettop'))[0]) : 0;
-
-    var distance = target.offset().top - headerOffset - offsetTop;
-      $('html, body').animate({
-        scrollTop: distance + 1
-      }, 1000, function () {});
-   }
 });
 
 $(window).on("resize", function () {
