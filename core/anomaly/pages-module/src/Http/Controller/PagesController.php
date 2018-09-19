5706db6c64b02ad562a886e18d7659c96738b7fb
cris <cristinag@flyingpigs.es>
2018-09-19 14:16:55 +0200

    core/anomaly/pages-module/src/Http/Controller/PagesController.php
    añado condicional para redirecciones dentro de los archivos de pyrocms.

--- core/anomaly/pages-module/src/Http/Controller/PagesController.php
+++ core/anomaly/pages-module/src/Http/Controller/PagesController.php
@@ -31,6 +31,92 @@ class PagesController extends PublicController
             abort(404);
         }
 
+        /* inicio codio de redirecciones */
+        if( $page->getPath() == "/" && count($_GET) > 0 ){
+
+            $rutas_old = [
+                'empresa' => [
+                    'historia',
+                    'nuestra-filosofia',
+                    'politica-empresa',
+                    'calidad',
+                    'responsabilidad-social-corporativa',
+                ],
+
+                'servicios' => [
+                    'servicios',
+                    'transporte-terrestre',
+                    'transporte-maritimo',
+                    'transporte-aereo',
+                    'aduanas',
+                    'proyectos',
+                    'almacenes',
+                    'seguros',
+                    'cross-trade',
+                ],
+
+                'noticias' => [
+                    'noticias',
+                ],
+
+                'contacto' => [
+                    'contacto',
+                ],
+
+                'inicio' => [
+                    'inicio',
+                ],
+
+            ];
+
+            $rutas = [
+                'inicio' => [
+                    'es' => "/inicio",
+                    'en' => "/home",
+                ],
+                'servicios' => [
+                    'es' => "/servicios",
+                    'en' => "/services",
+                ],
+                'empresa' => [
+                    'es' => "/empresa",
+                    'en' => "/company",
+                ],
+                'noticias' => [
+                    'es' => "/noticias",
+                    'en' => "/news",
+                ],
+                'contacto' => [
+                    'es' => "/contacto",
+                    'en' => "/contact",
+                ],
+
+            ];
+
+            if( isset( $_GET['p'] ) && $_GET['p'] != '' ){
+                if( $_GET['p'] == "utilidades" ){
+                    abort(404);
+                }
+
+                foreach ( $rutas_old as $k => $value ) {
+                    //var_dump ( $rutas_old[$k] );
+
+                    if (in_array($_GET['p'], $rutas_old[$k])) {
+                        //var_dump("ruta: " . $k);
+
+                        if (isset($_GET['i'])) {
+                            return redirect('/en'.$rutas[$k]['en'], 301);
+                        } else {
+                            return redirect($rutas[$k]['es'], 301);
+                        }
+                    }
+                }
+                return redirect('/', 301);
+            }
+
+        }
+        /* fin codio de redirecciones */
+
         $page->setCurrent(true);
         $page->setActive(true);
 
